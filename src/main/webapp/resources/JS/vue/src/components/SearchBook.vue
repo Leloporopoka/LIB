6a3b0ff359d4c585d8c16ae0b1d2fd4e5567864f
Nikita <duskinn02@gmail.com>
2019-04-25 00:17:37 +0600

    vue

--- src/main/webapp/resources/JS/vue/src/components/SearchBook.vue
+++ src/main/webapp/resources/JS/vue/src/components/SearchBook.vue
@@ -1,48 +1,73 @@
 <template>
-  <div>
-  <div class="search resultSearch">
-    <form action="search" class="search-container">
+    <div>
+        <div class="search resultSearch">
+            <form action="search" class="search-container">
 
-      <input type="text" class="form-control" name="search" v-model="search">
-
-
-      <input hidden name="tag" value="" id="tag_input">
-      <button type="submit" class="button search-icon">
-        <img src="resources\pic\search.png" alt="">
-      </button>
-    </form>
+                <input type="text" class="form-control" name="search" v-model="search">
 
 
-  </div>
-  <!--<div class="search_result" v-if="answer !== null">-->
+                <input hidden name="tag" value="" id="tag_input">
+                <button type="submit" class="button search-icon">
+                    <img src="resources\pic\search.png" alt="">
+                </button>
+            </form>
 
-    <!--<div class="result" v-for="book in answer.books " :id="book.id">-->
-      <!--<div class="media">-->
-        <!--{{book.name}}-->
-      <!--</div>-->
-    <!--</div>-->
-  <!--</div>-->
-  </div>
+        </div>
+        <div :key="displaykey">
+            <div class="search_result" v-if="!answer" >
+
+                <div class="result" v-for="book in answer.books " :id="book.id">
+                    <div class="media">
+                        <a :href="'book/' + book.id">
+                            <img height="150px" width="100px" :src="'data:image/jpeg;base64,'+ book.base64">
+                        </a>
+
+                        <div class="media-body">
+                            <a :href="'book/' + book.id"><h3>{{book.name}}</h3></a>
+                            <p>By {{book.author}}</p>
+                            <p>{{book.edition}} edition</p>
+                        </div>
+                        <div v-if="book.amount==0" class="available">
+                            <span>X</span>
+                            Not available
+                        </div>
+                        <div v-else class="available">
+                            <span>&#10003</span>
+                            Available
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-      name: "search-component",
+        name: "search-component",
+
         data() {
             return {
                 answer: null,
-                search:null
+                search: null,
+                displaykey: 0
+
             }
         },
-        mounted() {
-            this.loadData('http://' + location.host + '/getSearchBook');
+        mounted () {
+            this.loadData('/api/getAllBooks');
         },
-        methods:{
+        methods: {
             loadData(url) {
-                this.$axios.get(url).then(response => {
+              return  this.$axios.get(url).then(response => {
                     this.answer = response.data;
+                    this.displaykey++;
+                    this.loading = false;
+
                 });
+
             }
+
         }
     }
 
