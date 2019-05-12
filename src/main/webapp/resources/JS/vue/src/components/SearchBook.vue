0735ceebe23e8da9bfa314c13378d852a6a47ddd
Nikita <duskinn02@gmail.com>
2019-05-12 19:30:40 +0600

    Search fix and add tags in search page

--- src/main/webapp/resources/JS/vue/src/components/SearchBook.vue
+++ src/main/webapp/resources/JS/vue/src/components/SearchBook.vue
@@ -1,31 +1,38 @@
 <template>
+    <div class="row">
+        <div class="search_result col-9" v-if="answer !=undefined">
 
-    <div class="search_result" v-if="answer !=undefined">
+            <div class="result" v-for="book in filteredList " :id="book.id">
+                <div class="media">
+                    <a :href="'book/' + book.id">
+                        <img height="150px" width="100px" :src="'data:image/jpeg;base64,'+ book.base64">
+                    </a>
 
-        <div class="result" v-for="book in filteredList " :id="book.id">
-            <div class="media">
-                <a :href="'book/' + book.id">
-                    <img height="150px" width="100px" :src="'data:image/jpeg;base64,'+ book.base64">
-                </a>
-
-                <div class="media-body">
-                    <a :href="'book/' + book.id"><h3>{{book.name}}</h3></a>
-                    <p>By {{book.author}}</p>
-                    <p>{{book.edition}} edition</p>
+                    <div class="media-body">
+                        <a :href="'book/' + book.id"><h3>{{book.name}}</h3></a>
+                        <p>By {{book.author}}</p>
+                        <p>{{book.edition}} edition</p>
+                    </div>
+                    <div v-if="book.amount==0" class="available">
+                        <!--<span>X</span>-->
+                        Not available
+                    </div>
+                    <div v-else class="available">
+                        <!--<span>&#10003</span>-->
+                        Available
+                    </div>
                 </div>
-                <div v-if="book.amount==0" class="available">
-                    <!--<span>X</span>-->
-                    Not available
-                </div>
-                <div v-else class="available">
-                    <!--<span>&#10003</span>-->
-                    Available
+            </div>
+        </div>
+        <div v-else class="noBook col-9"></div>
+        <div class="col-3">
+            <div class="search-tags" :key="displaykey" v-if="tags != null">
+                <div class="single-search-tag" :id="tag.id" v-for="tag in tags">
+                    <i>+</i><i>-</i>{{tag.name}}
                 </div>
             </div>
         </div>
     </div>
-    <div v-else  class="noBook"></div>
-
 
 </template>
 
@@ -39,21 +46,33 @@
 
         data() {
             return {
+                tagsUrl: '/api/getAllTags',
                 answer: this.loadData,
+                tags: null,
+                displayKey: 0
             }
+        },
+        created() {
+            this.loadTags(this.tagsUrl);
+        },
+
+        methods: {
+            loadTags(url) {
+                return this.$axios.get(url).then(response => {
+                    this.tags = response.data;
+                    this.displaykey++;
+                });
+
+            },
         },
 
         computed: {
             filteredList() {
-                if(this.search == undefined)
+                if (this.search == undefined)
                     this.search = "";
                 return this.answer.filter(post => {
                     return post.name.toLowerCase().includes(this.search.toLowerCase())
                 })
-
-                    // .push(this.answer.filter(post => {
-                    //     return post.author.toLowerCase().includes(this.search.toLowerCase())
-                    // }))
             }
 
 
